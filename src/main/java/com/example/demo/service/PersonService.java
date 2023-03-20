517f4e9de0165e195bad599298e9a4be4065b8d3
Vijay Biradar <vijaybiradar8273@gmail.com>
2023-03-20 11:21:21 +0530

    o

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,6 @@ public class PersonService {
     
     private final PersonDao personDao;
 
-@Autowired
 public PersonService(@Qualifier("fakedao") 
     PersonDao personDao) {
     
